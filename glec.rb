ea7be4adb7bcc5f514e674e539799e1fa2299e0d
jz4o <26317360+jz4o@users.noreply.github.com>
2017-10-12 23:03:39 +0900

    ユーザで絞込みができるよう修正

--- glec.rb
+++ glec.rb
@@ -5,8 +5,25 @@
 require 'optparse'
 require 'net/https'
 require 'uri'
+require 'json'
 
 Glec = Module.new do
+  class String
+    def to_array_of_hash
+      JSON.parse(self)
+    end
+  end
+
+  class Array
+    def refine_by_user(user)
+      unless user.eql? TARGET_ALL
+        self.select!{ |event| event['actor']['login'].eql? user }
+      end
+
+      self
+    end
+  end
+
   # GithubのAPIを呼び出し、結果を返す
   def self.get_events(owner: , repo: )
     url = "https://api.github.com/repos/#{owner}/#{repo}/events"
@@ -33,19 +50,24 @@ Glec = Module.new do
   def self.start(params)
     repo_data = params.select{ |key| %i[owner repo].include? key }
     puts Glec.get_events(repo_data)
+      .to_array_of_hash
+      .refine_by_user(params[:user])
   rescue => e
     puts e.message
   end
 end
 
 
+TARGET_ALL    = 'all'
 DEFAULT_OWNER = 'jz4o'
 DEFAULT_REPO  = 'glec'
+DEFAULT_USER  = TARGET_ALL
 
 params = ARGV.getopts(
   '',
   "owner:#{DEFAULT_OWNER}",
-  "repo:#{DEFAULT_REPO}"
+  "repo:#{DEFAULT_REPO}",
+  "user:#{DEFAULT_USER}"
 ).map{ |k,v| [k.to_sym, v] }.to_h
 
 Glec.start(params)
